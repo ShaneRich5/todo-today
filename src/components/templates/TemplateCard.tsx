c37524192a847d7df018116845bb5e9abe6f5f03
Shane Richards <shane.richards121@gmail.com>
2024-03-26 01:07:09 -0400

    feat: building out the template page

---
+++ src/components/templates/TemplateCard.tsx
@@ -0,0 +1,15 @@
+const TemplateCard = () => {
+  return (
+    <div className="card bg-orange-300 text-primary-content">
+      <div className="card-body">
+        <h2 className="card-title">Template A</h2>
+        <p>Description here</p>
+        <div className="card-actions justify-end">
+          <button className="btn">View</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TemplateCard;
--- src/pages/TaskTemplateListPage.tsx
+++ src/pages/TaskTemplateListPage.tsx
@@ -1,7 +1,15 @@
+import TemplateCard from '../components/templates/TemplateCard';
+
 const TaskTemplateListPage = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <h1>Task Template List</h1>
+      <div className="grid grid-cols-3 gap-4">
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+      </div>
     </div>
   );
 };
